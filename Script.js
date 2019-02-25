88e8b0b331d3226fd1cbd7375a9b4be8ec9673df
cherin <c4cheriachan@gmail.com>
2019-02-25 16:30:41 -0500

    4.30 commit

--- Script.js
+++ Script.js
@@ -3,6 +3,12 @@ function addNumbers()
 {
     var bird = { genus: "corvus", species: "corvax", commonName: "raven", callType: "squawky", quote: "nevermore", maxOffSpring: 5, noisy: true, deadly: false };
     var bear = { genus: "ursus", species: "arctus", commonName: "brown bear", callType: "roar", quote: "", maxOffSpring: 3, noisy: true, deadly: true };
+    var person = {
+        FirstName: "John",
+        LastName: "Kora",
+        Age: 25,
+        Gender: "Male"
+    };
 
 }
 
