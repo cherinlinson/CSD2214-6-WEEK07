082eef995c35c191bd88f701cc30fc389ea6bcbe
cherin <c4cheriachan@gmail.com>
2019-02-25 15:30:54 -0500

    3.30 commit

--- Script.js
+++ Script.js
@@ -1,11 +1,17 @@
 // JavaScript source code
 function addNumbers()
 {
-    var sum = 0;
-    for (i = 117; 1 <= 11;)
-    {
-        sum += i;
-        i -= 8;
-    }
-    return sum;
+    var bird = { genus: "corvus", species: "corvax", commonName: "raven", callType: "squawky", quote: "nevermore", maxOffSpring: 5, noisy: true, deadly: false };
+    var bear = { genus: "ursus", species: "arctus", commonName: "brown bear", callType: "roar", quote: "", maxOffSpring: 3, noisy: true, deadly: true };
+
 }
+
+
+
+
+
+
+
+
+
+
